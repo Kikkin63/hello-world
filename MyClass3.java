453f490f6eaee3f2c4d361f080874e9bf3f6222f
Kristin Wenhaug <50317668+Kikkin63@users.noreply.github.com>
2019-05-21 14:37:29 +0200

    Add files via upload

---
+++ MyClass3.java
@@ -0,0 +1,19 @@
+/**
+ * Oppgave1 med frist mandag 3 juni: Alle skal ha skrevet et ‘Hello World’-program der du også legger sammen
+ * to tall og skriver ut svaret, samt sjekker dette inn på gruppens Github-repo ‘Faggruppe-Javafb2019’.
+ *
+ * @author  Kristin
+ * @version 2.0
+ * @date    21.05.2019
+ */
+
+
+
+public class MyClass3 {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    int x = 15;
+    int y = 16;
+    System.out.println(x + y); // Print summen av x + y
+  }
+}
